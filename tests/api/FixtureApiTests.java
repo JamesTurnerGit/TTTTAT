16fa18a27352d180a62278c6a0095dca9dd4581b
jamesT@saglobal.com <james.turner@lexisnexis.co.uk>
2019-07-09 00:50:36 +0100

    started work on adding fixtures

--- tests/api/FixtureApiTests.java
+++ tests/api/FixtureApiTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
-class FixtureApiTest {
+class FixtureApiTests {
 
     FixtureApi api;
 
---
+++ tests/api/StoreFixtureApiTests.java
@@ -0,0 +1,20 @@
+package api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class StoreFixtureApiTests {
+    FixtureApi api;
+
+    @BeforeEach
+    void init(){
+        this.api = new FixtureApi();//overload this contructor to point tests elsewhere, would normally take from an ENV
+    }
+}
+
+
