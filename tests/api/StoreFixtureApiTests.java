a63f1835e82435a48a49a8fa17aacc56704e6708
jamesT@saglobal.com <james.turner@lexisnexis.co.uk>
2019-07-09 01:39:12 +0100

    added mock data

--- tests/api/StoreFixtureApiTests.java
+++ tests/api/StoreFixtureApiTests.java
@@ -1,6 +1,6 @@
 package api;
 
-import api.models.Fixture;
+import api.models.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +15,25 @@ public class StoreFixtureApiTests {
     @BeforeEach
     void init(){
         this.api = new FixtureApi();//overload this contructor to point tests elsewhere, would normally take from an ENV
+    }
+
+    @Test
+    void CanStoreAFixtureAndRetrieveit()
+    {
+        Goal[] goals= new Goal[0];
+        goals[0] = new Goal(1400,true,1,false,false,"FIRST_HALF",6999, "1");;
+
+        Team[] teams= new Team[1];
+        teams[0] =  new Team ( "HOME", "chipmunks","HOME");
+        teams[1] = new Team ( "AWAY", "chippettes","AWAY");
+
+        FootballFullState footballFullState = new FootballFullState("chipmunks","chippettes",false, 2000,goals,"FIRST_HALF","1995-03-20T10:49:38.655Z",true,teams);
+
+        FixtureStatus fixtureStatus = new FixtureStatus(true,false);
+
+        Fixture fixture = new Fixture(1,fixtureStatus,footballFullState);
+
+        //api.storeFixture(fixture);
     }
 }
 
